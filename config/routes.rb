5ff52f98bbe2b372d80fc226346e8c153535c45d
MasaoSasaki <daikichi852@icloud.com>
2022-01-15 14:31:36 +0900

    🚧ルーティング順序の変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'homes#top'
-  get 'users/edit'
   devise_for :users
+  resources :users, only: %i[show edit update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
