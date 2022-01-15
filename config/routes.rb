bae961fd3e1d44e449119081755c5414870a3031
MasaoSasaki <daikichi852@icloud.com>
2022-01-15 17:03:37 +0900

    🚧 Add controller Posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'homes#top'
   devise_for :users
   resources :users, only: %i[show edit update]
+  resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
