eb421200cbae130893af2db97c33683a3b16f7ef
MasaoSasaki <daikichi852@icloud.com>
2022-01-16 13:53:28 +0900

    🚧 postテーブルのuser_idにindexを追加しました

--- db/migrate/20220115054645_create_posts.rb
+++ db/migrate/20220115054645_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.integer :user_id, null: :false
-      t.text :body
+      t.belongs_to :user, null: :false
+      t.text :body, optional: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 2022_01_15_054645) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
