1ca36a696d8ff7a99c97bf80ba674a268a4ffa56
MasaoSasaki <daikichi852@icloud.com>
2022-01-17 20:32:12 +0900

    🚧 投稿を非同期にしました

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,30 +1,38 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @post = Post.new
+    @posts = Post.all.order(id: "DESC")
   end
   def show
   end
   def new
-    @post = Post.new
   end
   def edit
   end
   def create
     post = Post.new(post_params)
     post.user_id = current_user.id
-    @posts = Post.all
     if post.save
-      redirect_to "/posts"
+      render turbo_stream: turbo_stream.replace(
+        'post-list',
+        partial: 'posts/post_list',
+        locals: {posts: Post.all.order(id: "DESC")}
+      )
     else
-      render :new
+      p "NG"
     end
   end
   def update
   end
   def destroy
     post = Post.find(params[:id])
-    post.destroy
-    redirect_to "/posts"
+    if post.destroy
+      render turbo_stream: turbo_stream.replace(
+        'post-list',
+        partial: 'posts/post_list',
+        locals: {posts: Post.all.order(id: "DESC")}
+      )
+    end
   end
 
   private
