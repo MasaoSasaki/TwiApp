46da83e84d17c4103dfb792a3021e568c775c438
MasaoSasaki <daikichi852@icloud.com>
2022-01-19 16:53:27 +0900

    ♻️ Refactoring

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,31 +1,24 @@
 class PostsController < ApplicationController
+
   def index
     @post = Post.new
     @posts = Post.all.order(id: "DESC")
   end
-  def show
-    @post = Post.find(params[:id])
-  end
+
   def new
     @post = Post.new
   end
-  def edit
-    @post = Post.find(params[:id])
-  end
+
   def create
     @post = Post.new(post_params)
-    p post_params
     if @post.save
       respond_to do |format|
         format.turbo_stream
         format.html { redirect_to posts_path }
       end
-    else
-      p "NG"
     end
   end
-  def update
-  end
+
   def destroy
     @post = Post.find(params[:id])
     if @post.destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root 'homes#top'
   devise_for :users
-  resources :users, only: %i[show edit update]
-  resources :posts
+  resources :posts, except: %i[show edit update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
