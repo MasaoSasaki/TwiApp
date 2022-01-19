1c34b000dd34328245127d5896fb9a691de0a100
MasaoSasaki <daikichi852@icloud.com>
2022-01-19 16:37:15 +0900

    🚧 削除処理の自動更新を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,19 +7,19 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
   def new
+    @post = Post.new
   end
   def edit
     @post = Post.find(params[:id])
   end
   def create
-    post = Post.new(post_params)
-    post.user_id = current_user.id
-    if post.save
-      render turbo_stream: turbo_stream.replace(
-        'post-list',
-        partial: 'posts/post_list',
-        locals: {posts: Post.all.order(id: "DESC"), post: Post.new}
-      )
+    @post = Post.new(post_params)
+    p post_params
+    if @post.save
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to posts_path }
+      end
     else
       p "NG"
     end
@@ -27,19 +27,15 @@ class PostsController < ApplicationController
   def update
   end
   def destroy
-    post = Post.find(params[:id])
-    if post.destroy
-      render turbo_stream: turbo_stream.replace(
-        'post-list',
-        partial: 'posts/post_list',
-        locals: {posts: Post.all.order(id: "DESC"), post: Post.new}
-      )
+    @post = Post.find(params[:id])
+    if @post.destroy
+      render :destroy
     end
   end
 
   private
   def post_params
-    params.require(:post).permit(:body)
+    params.require(:post).permit(:user_id, :body)
   end
 
 end
